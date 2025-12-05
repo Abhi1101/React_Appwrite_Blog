ceaea2eafef75b6cc51753629a677787e675db71
Abhi Chauhan <abhichauhan1101@gmail.com>
2025-12-05 19:30:22 +0530

    tinyMCE api key changed

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -14,7 +14,7 @@ export default function RTE({ name, control, label, defaultValue = "" }) {
         control={control}
         render={({ field: { onChange } }) => (
           <Editor
-            apiKey='iehx8aqf0p8xznmcq5ojdkysl7srgovqwwo77mgcxfa6i6u9'
+            apiKey='6uwj9kgn20g2fbe8kwnkmi719nspoz5i7cqknh1xj98zuxy8'
             initialValue={defaultValue}
             init={{
               initialValue: defaultValue,
